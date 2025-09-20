9deaf2be0673c5f2e5a3b01c1861a3b70fa413b1
Ural-Dodo <shakurovural@bk.ru>
2025-09-20 14:27:02 +0500

    Убрал строку javascript из начала кода

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-javascript
+
 document.getElementById('add-note-btn').addEventListener('click', function(){
     const noteInput = document.getElementById('note-input');
     const notesContainer = document.getElementById('notes-conteiner');
